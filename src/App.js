2c153957d7a4c4fe6a8ccce10c51ee96ff340bda
heyimcsy <cinnamon1995@naver.com>
2023-02-27 10:03:57 +0900

    css파일에서 실행시킨 스타일링

--- src/App.js
+++ src/App.js
@@ -2,28 +2,14 @@ import React from 'react'
 import './App.css';
 
 function App(){
-  const style = {
-    padding: "100px",
-    display: "flex",
-    gap: "12px"
-  }
-  const boxStyle = {
-    width : "100px",
-    height: "100px",
-    fontSize: "25px",
-    border : "3px solid rgb(200, 168, 248)",
-    borderRadius: "10px",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  }
+
   return (
-    <div style={style}>
-      <div style={boxStyle}>감자</div>
-      <div style={boxStyle}>고구마</div>
-      <div style={boxStyle}>오이</div>
-      <div style={boxStyle}>가지</div>
-      <div style={boxStyle}>옥수수</div>
+    <div className='app-style'>
+      <div className='box-style'>감자</div>
+      <div className='box-style'>고구마</div>
+      <div className='box-style'>오이</div>
+      <div className='box-style'>가지</div>
+      <div className='box-style'>옥수수</div>
     </div>
   );
 }
